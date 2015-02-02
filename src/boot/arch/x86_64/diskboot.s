cd372a9b5b1a4c25463c5ddc4714da0c5b829a09
Hirochika Asai <panda@jar.jp>
2015-02-02 18:07:25 +0900

    support to display a null-terminated string.

--- src/boot/arch/x86_64/diskboot.s
+++ src/boot/arch/x86_64/diskboot.s
@@ -13,5 +13,36 @@
 	.globl	start
 
 start:
+	/* Display the welcome message */
+	movw	$msg_welcome,%si	/* %ds:(%si) -> welcome message */
+	call	putstr
+
+	/* Halt */
 	cli
 	hlt
+
+
+/* Display a null-terminated string */
+putstr:
+putstr.load:
+	lodsb			/* Load %al from %ds:(%si), then incl %si */
+	testb	%al,%al		/* Stop at null */
+	jnz	putstr.putc	/* Call the function to output %al */
+	ret			/* Return if null is reached */
+putstr.putc:
+	call	putc		/* Output a character %al */
+	jmp	putstr		/* Go to next character */
+putc:
+	pushw	%bx		/* Save %bx */
+	movw	$0x7,%bx	/* %bh: Page number for text mode */
+				/* %bl: Color code for graphics mode */
+	movb	$0xe,%ah	/* BIOS: Put char in tty mode */
+	int	$0x10		/* Call BIOS, print a character in %al */
+	popw	%bx		/* Restore %bx */
+	ret
+
+	.data
+
+msg_welcome:
+	.ascii	"Welcome to Academic Operating System!\r\n\n"
+	.asciz	"Let's get it started.\r\n\n"
