288d165d692e13c9610e3af2fdf5daf1b8eaf54e
Hirochika Asai <panda@jar.jp>
2015-02-11 17:46:22 +0900

    implement dist read (multiple sectors) functions.

--- src/boot/arch/x86_64/diskboot.s
+++ src/boot/arch/x86_64/diskboot.s
@@ -21,8 +21,12 @@
  * SOFTWARE.
  */
 
-	.set	BOOTMON_SEG,0x0900      /* Memory where to load boot monitor */
-	.set	BOOTMON_OFF,0x0000      /*  segment and offset [0900:0000] */
+	.set	BOOTMON_SEG,0x0900	/* Memory where to load boot monitor */
+	.set	BOOTMON_OFF,0x0000	/*  segment and offset [0900:0000] */
+	.set	BOOTMON_SIZE,0x000f	/* Boot monitor size in sector */
+	.set	NUM_RETRIES,3		/* # of retries for disk read */
+	.set	ERRCODE_TIMEOUT,0x80	/* Error code: Timeout */
+	.set	SECTOR_SIZE,0x200	/* 512 bytes / sector */
 
 	.text
 
@@ -33,30 +37,162 @@ start:
 	/* Save drive information */
 	movb	%dl,drive
 
-	/* Read a sector from the drive */
+	cld			/* Clear direction flag */
+				/*  (inc di/si for str ops) */
+
+	/* Setup stack */
+	cli			/* Disable interrupts */
+	xorw	%ax,%ax
+	movw	%ax,%ss		/* Stack segment (%ss=0) */
+	movw	%ax,%ds		/* Data segment (%ds=0) */
+	movw	%ax,%es		/* Data segment (%es=0) */
+	sti			/* Disable interrupts */
+
+	/* Get drive parameters */
+get_drive_params:
+	xorw	%ax,%ax
+	movw	%ax,%es		/* Set %es:%di */
+	movw	%ax,%di		/*  to 0x0000:0x0000 */
+	movb	$0x08,%ah	/* Function: Read drive parameter */
+	int	$0x13
+	jc	read_error	/* Error on read */
+	/* Save the sector information */
+	incb	%dh		/* Get # of heads (%dh: last index of heads) */
+	movb	%dh,heads	/* Store */
+	movb	%cl,%al		/* %cl[5:0]: last index of sectors per track */
+	andb	$0x3f,%al	/*  N.B., sector starting with 1 */
+	movb	%al,sectors	/* Store */
+	movb	%ch,%al		/* %cx[7:6]%cx[15:8]: last index of cylinders */
+				/*  then copy %cx[15:8] to %al */
+	movb	%cl,%ah		/* Lower byte to higher byte */
+	shrb	$6,%ah		/* Pick most significant two bits */
+	incw	%ax		/*  N.B., cylinder starting with 0 */
+	movw	%ax,cylinders
+
+	movb	drive,%dl
+	movw	$BOOTMON_SIZE,%cx	/* Specify boot monitor size */
+	movw	$1,%ax
 	movw	$BOOTMON_SEG,%bx
 	movw	%bx,%es		/* Buffer address pointer (Segment) */
 	movw	$BOOTMON_OFF,%bx	/* Buffer address pointer (Offset) */
-	movb	$0x02,%ah	/* Function: Read sectors from drive */
-	movb	$0x01,%al	/* # of sectors to be read */
-	movw	$0x0002,%cx	/* Cylinder[6:15] | Sector[0:5] */
-	movb	$0x00,%dh	/* Head */
-	movb	drive,%dl	/* Drive */
-	int	$0x13
+	call	read		/* Read %cx sectors starting at LBA %ax on */
+				/*  drive %dl into %es:[%bx] */
 
-	/* Check the status code */
-	testb	%ah,%ah
-	jnz	1f
 	/* Jump to boot monitro*/
 	ljmp	$BOOTMON_SEG,$BOOTMON_OFF
 
+
+
+/* Read %cx sectors starting at LBA %ax on drive %dl into %es:[%bx] */
+read:
+	pushw	%bp		/* Save the base pointer*/
+	movw	%sp,%bp		/* Copy the stack pointer to the base pointer */
+	/* Save general purpose registers */
+	movw	%ax,-2(%bp)
+	movw	%bx,-4(%bp)
+	movw	%cx,-6(%bp)
+	movw	%dx,-8(%bp)
+	/* Prepare space for local variables */
+	/* u16 cx -10(%bp) */
+	/* u16 counter -12(%bp) */
+	subw	$12,%sp
+
+	/* Reset counter */
+	xorw	%ax,%ax
+	movw	%ax,-12(%bp)
+
+	/* Set number of sectors to be read */
+	movw	%cx,-10(%bp)
 1:
-	/* Error */
-	movw	$msg_error,%si	/* %ds:(%si) -> error message */
-	call	putstr
-	/* Halt */
-	cli
-	hlt
+	movw	-2(%bp),%ax	/* Restore %ax */
+	addw	-12(%bp),%ax	/* Current LBA */
+	call	lba2chs		/* Convert LBA (%ax) to CHS (%cx,%dh) */
+	call	read_sector	/* Read a sector */
+	addw	$SECTOR_SIZE,%bx
+	movw	-12(%bp),%ax	/* Get */
+	incw	%ax		/*  and increase the current LBA %ax */
+	movw	%ax,-12(%bp)	/*  then write back */
+	cmpw	-10(%bp),%ax
+	jb	1b		/* Need to read more sectors */
+
+	/* Restore the saved registers */
+	movw	-8(%bp),%dx
+	movw	-6(%bp),%cx
+	movw	-4(%bp),%bx
+	movw	-2(%bp),%ax
+	movw	%bp,%sp		/* Restore the stack pointer and base pointer */
+	popw	%bp
+	ret
+
+
+/* Read one sector from CHS (specified by %dh and %cx) specified on drive %dl to
+ * %es:[%bx]
+ */
+read_sector:
+	pushw	%bp		/* Save the base pointer*/
+	movw	%sp,%bp		/* Copy the stack pointer to the base pointer */
+	/* Save registers */
+	movw	%ax,-2(%bp)
+	/* Prepare space for local variables */
+	/* u16 retries -4(%bp) */
+	/* u16 error -6(%bp) */
+	subw	$6,%sp
+	/* Reset retry counter */
+	xorw	%ax,%ax
+	movw	%ax,-4(%bp)
+1:
+	/* Read a sector from the drive */
+	movb	$0x02,%ah	/* Function: Read sectors from drive */
+	movb	$0x01,%al	/* # of sectors to be read */
+	int	$0x13
+	jnc	2f		/* Jump if success */
+	movw	%ax,-6(%bp)	/* Save the return code */
+	movw	-4(%bp),%ax	/* Get */
+	incw	%ax		/*  and increase the number of retries */
+	movw	%ax,-4(%bp)	/*  then write back */
+	cmpw	$NUM_RETRIES,%ax
+	movw	-6(%bp),%ax	/* Restore the return code */
+	ja	read_error	/* Exceeding the maximum number of retries */
+	cmpb	$ERRCODE_TIMEOUT,%ah
+	je	read_error	/* Timeout */
+2:
+	/* Restore the saved registers */
+	movw	-2(%bp),%ax
+	movw	%bp,%sp		/* Restore the stack pointer and base pointer */
+	popw	%bp
+	ret
+
+
+/* Calculate CHS (%cx[7:6]%cx[15:8] ,%dh, %cx[5:0]) from LBA (%ax) */
+lba2chs:
+	/* Save registers */
+	pushw	%ax
+	pushw	%bx
+	pushw	%dx
+	/* Compute sector number */
+	xorw	%bx,%bx
+	movw	%bx,%dx
+	movw	%bx,%cx
+	movb	sectors,%bl
+	divw	%bx		/* %dx:%ax / %bx; %ax:quotient, %dx:remainder */
+	incb	%dl		/* Sector number is one-based numbering */
+	movb	%dl,%cl		/* Sector: %cx[5:0] */
+	/* Compute head and track (cylinder) numbers */
+	xorw	%bx,%bx
+	movw	%bx,%dx
+	movb	heads,%bl
+	divw	%bx		/* %dx:%ax / %bx; %ax:cylinder, %dx:head */
+	movb	%al,%ch		/* Cylinder[7:0]: %cx[15:8] */
+	movb	%ah,%bl		/* Take the least significant two bits */
+	shlb	$6,%bl		/*  from %ah, and copy to %bl[7:6] */
+	orb	%bl,%cl		/* Cylinder[9:8]: %cx[7:6]*/
+	movw	%dx,%bx		/* Save the remainer to %bx */
+	popw	%dx		/* Restore %dx */
+	movb	%bl,%dh		/* Head */
+	/* Restore registers */
+	popw	%bx
+	popw	%ax
+	ret
 
 
 /* Display a null-terminated string */
@@ -78,10 +214,50 @@ putc:
 	popw	%bx		/* Restore %bx */
 	ret
 
+/* Display the read error message (%ah = error code) */
+read_error:
+	movb	%ah,%al
+	movw	$error_code,%di
+	xorw	%bx,%bx
+	movw	%bx,%es
+	call	hex8
+	movw	$msg_error,%si	/* %ds:(%si) -> error message */
+	call	putstr		/* Display error message at %si and then halt */
+
+/* Halt */
+halt:
+	hlt
+	jmp	halt
+
+
+/* Convert %al to hex characters, saving the result to [%di] */
+hex8:
+	pushw	%ax		/* Save %ax */
+	shrb	$0x4,%al	/* Get most significant 4 bits in %al */
+	call	hex8.allsb4	/* Convert the least significant 4 bits in */
+				/*  %al to a hex character */
+	popw	%ax		/* Restore %ax */
+hex8.allsb4:
+	andb	$0xf,%al	/* Get least significant 4 bits in %al */
+	cmpb	$0xa,%al	/* CF=1 if %al < $0xa (0..9) */
+	sbbb	$0x69,%al	/* %al <= %al - ($0x69 + CF) */
+	das			/* BCD (N.B., %al - 0x60 if AF is not set) */
+	orb	$0x20,%al	/* To lower case */
+	stosb			/* Save char to %es:[%di] and inc %di */
+	ret
+
 	.data
 
 msg_error:
-	.asciz	"Disk read error!\r\n"
+	.ascii  "Disk read error: 0x"
+error_code:
+        .asciz  "00\r\r"
 
 drive:
+	.byte	0
+heads:
+	.byte	0
+cylinders:
+	.word	0
+sectors:
 	.byte	0
