c7db6c8750fd0add72ed016785ae9d8c1dab1f6b
Hirochika Asai <panda@jar.jp>
2015-06-14 03:00:11 +0900

    fix a bug in the stack and screen initialization procedure.

--- src/boot/arch/x86_64/diskboot.s
+++ src/boot/arch/x86_64/diskboot.s
@@ -38,19 +38,21 @@ start:
 	cld			/* Clear direction flag */
 				/*  (inc di/si for str ops) */
 
-	/* Setup stack */
 	cli			/* Disable interrupts */
 	xorw	%ax,%ax
+	/* Setup stack */
 	movw	%ax,%ss		/* Stack segment (%ss=0) */
-	movw	%ax,%ds		/* Data segment (%ds=0) */
-	movw	%ax,%es		/* Data segment (%es=0) */
+	movw	$start,%sp	/* Stack pointer */
+	/* Reset data segment registers */
+	movw	%ax,%ds		/* %ds=0 */
+	movw	%ax,%es		/* %es=0 */
 	sti			/* Enable interrupts */
 
 	/* Save drive information */
 	movb	%dl,drive
 
 	/* Set video mode to 16bit color text mode */
-	movb	VGA_TEXT_COLOR_80x25,%al
+	movb	$VGA_TEXT_COLOR_80x25,%al
 	movb	$0x00,%ah
 	int	$0x10
 
