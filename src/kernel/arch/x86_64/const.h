111677e500d16936683f5db9bc114f46980d3956
Hirochika Asai <panda@jar.jp>
2016-04-13 20:30:08 +0900

    Move the constant values for assembly files to const.h.

---
+++ src/kernel/arch/x86_64/const.h
@@ -0,0 +1,79 @@
+/*_
+ * Copyright (c) 2016 Hirochika Asai <asai@jar.jp>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _KERNEL_CONST_H
+#define _KERNEL_CONST_H
+
+/* GDT selectors */
+#define GDT_RING0_CODE_SEL      0x08
+#define GDT_RING0_DATA_SEL      0x10
+#define GDT_RING3_CODE32_SEL    0x18
+#define GDT_RING3_DATA_SEL      0x20
+#define GDT_RING3_CODE64_SEL    0x28
+
+/* Temporary GDT for application processors */
+#define AP_GDT_CODE64_SEL       0x08    /* Code64 selector */
+#define AP_GDT_CODE32_SEL       0x10    /* Code32 selector */
+#define AP_GDT_CODE16_SEL       0x18    /* Code16 selector */
+#define AP_GDT_DATA64_SEL       0x20    /* Data selector */
+#define AP_GDT_DATA32_SEL       0x28    /* Data selector */
+
+#define APIC_BASE               0xfee00000
+
+/* # of interrupts */
+#define IDT_NR                  256
+
+/* Kernel page table */
+#define KERNEL_PGT              0x00079000
+/* Per-processor information (struct p_data) */
+#define P_DATA_BASE             0x01000000
+#define P_DATA_SIZE             0x10000
+#define P_STACK_GUARD           0x10
+#define P_TSS_SIZE              104     /* sizeof(struct tss) */
+#define P_TSS_OFFSET            (0x20 + IDT_NR * 8)     /* struct tss */
+#define P_CUR_TASK_OFFSET       (P_TSS_OFFSET + P_TSS_SIZE)     /* cur_task */
+#define P_NEXT_TASK_OFFSET      (P_CUR_TASK_OFFSET + 8) /* next_task */
+/* Task information (struct arch_task) */
+#define TASK_RP                 0
+#define TASK_SP0                8
+#define TASK_CR3                16
+#define TASK_KTASK              40
+/* TSS */
+#define TSS_SP0                 4
+/* Trampoline */
+#define TRAMPOLINE_VEC          0x70
+
+/* Syscall */
+#define SYSCALL_MAX_NR          0x10
+
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
