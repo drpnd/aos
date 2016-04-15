bb362c8e8e1c00c0ca987e6e0f6f49799a9b4abf
Hirochika Asai <panda@jar.jp>
2016-04-15 11:18:50 +0900

    Move the constant values for assembly files to const.h.

--- src/kernel/arch/x86_64/const.h
+++ src/kernel/arch/x86_64/const.h
@@ -66,6 +66,46 @@
 /* Syscall */
 #define SYSCALL_MAX_NR          0x10
 
+/* MSR */
+#define MSR_IA32_EFER           0xc0000080
+#define IA32_EFER_SCE           0               /* SysCall enable */
+#define IA32_EFER_LME           8               /* IA-32e mode enable */
+#define IA32_EFER_LMA           10              /* IA-32e mode active */
+#define IA32_EFER_NXE           11              /* Execute-disable bit enable */
+
+/* Control Registers */
+#define CR0_PE                  0
+#define CR0_MP                  1
+#define CR0_EM                  2
+#define CR0_TS                  3
+#define CR0_ET                  4
+#define CR0_NE                  5
+#define CR0_WP                  16
+#define CR0_AM                  18
+#define CR0_NW                  29
+#define CR0_CD                  30
+#define CR0_PG                  31
+
+#define CR3_PWT                 3
+#define CR3_PCD                 4
+
+#define CR4_VME                 0
+#define CR4_PVI                 1
+#define CR4_TSD                 2
+#define CR4_DE                  3
+#define CR4_PSE                 4
+#define CR4_PAE                 5
+#define CR4_MCE                 6
+#define CR4_PGE                 7
+#define CR4_PCE                 8
+#define CR4_OSFXSR              9
+#define CR4_OSXMMEXCPT          10
+#define CR4_VMXE                13
+#define CR4_SMXE                14
+#define CR4_FSGSBASE            16
+#define CR4_PCIDE               17
+#define CR4_OSXSAVE             18
+#define CR4_SMEP                20
 
 #endif
 
